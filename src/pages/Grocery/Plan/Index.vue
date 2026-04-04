df64e7f1d382d9a34321313f9c02020ee5560f1c
Michael Joseph Go <135100340+mike-jgo@users.noreply.github.com>
2026-04-04 21:25:23 +0800

    Update Index.vue

--- src/pages/Grocery/Plan/Index.vue
+++ src/pages/Grocery/Plan/Index.vue
@@ -200,7 +200,7 @@ const filteredSuggestions = computed(() => {
           type="number"
           min="0"
           step="any"
-          placeholder="Amount"
+          placeholder="0"
           class="w-20 border-none py-2 pl-3 pr-1 text-gray-900 placeholder-gray-400 focus:outline-none"
         />
         <select
